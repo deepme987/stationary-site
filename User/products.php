c4c089cdd551429c1067dcf69da3f490ac9ad7ef
mithil-jain <mithiljain8@gmail.com>
2018-09-22 19:28:45 +0530

    added products

--- User/products.php
+++ User/products.php
@@ -14,6 +14,11 @@
             flex-wrap: wrap;
             justify-content: space-around;
         }
+        img
+        {
+            width:100%;
+            height:300px;
+        }
         .card
         {
             margin-top:10px;
@@ -22,53 +27,42 @@
         {
             box-shadow:1px 1px 5px black;
         }
+        p
+        {
+            text-align: center;
+        }
+        input[type='number']
+        {
+            width: 50px;
+            margin-left: 10px;
+        }
         
     </style>
 </head>
 <body>
-    <div class="flex" >
-        <div class="card" style="width: 22rem;">
-          <img class="card-img-top" src="addons/fake.png" alt="Card image cap">
-          <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-            <button type="button" class="btn btn-primary">Primary</button>
-        </div>
-        <div class="card" style="width: 22rem;">
-          <img class="card-img-top" src="addons/fake.png" alt="Card image cap">
-          <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-            <button type="button" class="btn btn-primary">Primary</button>
-        </div>
-        <div class="card" style="width: 22rem;">
-          <img class="card-img-top" src="addons/fake.png" alt="Card image cap">
-          <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-            <button type="button" class="btn btn-primary">Primary</button>
-        </div>
-        <div class="card" style="width: 22rem;">
-          <img class="card-img-top" src="addons/fake.png" alt="Card image cap">
-          <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-            <a href="#"  class="btn btn-primary">Add to cart</a>
-        </div>
-        <div class="card" style="width: 22rem;">
-          <img class="card-img-top" src="addons/fake.png" alt="Card image cap">
-          <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-            <a href="#" class="btn btn-primary">Add to cart</a>
-        </div>
-        <div class="card" style="width: 22rem;">
-          <img class="card-img-top" src="addons/fake.png" alt="Card image cap">
-          <div class="card-body">
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          </div>
-            <a href="#" class="btn btn-primary">Add to cart</a>
-        </div>
+    <div class="flex">
+        <?php
+            $user = 'user';
+			$pass = 'sakec';
+			$db = 'stationary';
+
+			$conn = new mysqli('localhost', $user, $pass, $db) or die("Unable to connect to server".$db);
+            
+		    $get_all = "select * from products";
+            $data = mysqli_query($conn, $get_all) or die("No records found.");
+            while($row = mysqli_fetch_assoc($data))
+            {
+                echo "
+                    <div class='card' style='width: 22rem;'>
+                        <img class='card-img-top' src='addons\\".$row["Link"]."' alt='".$row["Pname"]."'>
+                        <div class='card-body'>
+                          <p class='card-text'>".$row["Pname"]."<br>Cost:₹".$row["Cost"]."<br>Quantity<input type='number'></p>
+                         </div>
+                        <button type='button' name='".$row['Pid']."cart' class='btn btn-primary'>Primary</button>
+                     </div>
+                    ";
+            }
+        ?>
     </div>
 
 </body>
